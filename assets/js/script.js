e9c8d124dfddbc374f67fc380f9b692741b1b487
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-03-12 20:58:14 -0600

    modififed function and added varaiables to get the basics of the projects down and have the prompts ask us for info.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,54 @@
 // Assignment code here
+  var spclCharacters = "!, #, $, %, &, /, :, <, =, ?, @, ], ~, _, >, ^";
+  var password = "";
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var char = "";
 
+
+function generatePassword(){
+
+  var passwordPrompt = window.prompt("Please choose password length, password must be between 8 and 128 characters long");
+
+  passwordLength = parseInt(passwordPrompt);
+
+  if(passwordLength < 8 || passwordLength> 128) {
+    window.alert("You need to provide an accepted length. Please try again.")
+    return generatePassword();
+  } else if (passwordPrompt === typeof("string")) {
+    return generatePassword();
+  }
+
+  var promptMe =  window.prompt("Type 1 for uppercase, 2  for lowercase, 3 for numbers, 4 for special charachters.");
+  var promptNum = parseInt(promptMe);
+  if (promptNum === 1){
+    char += letters.toUpperCase();
+    
+  } else if( promptNum === 2) {
+    char += letters;
+
+  } else if ( promptNum === 3) {
+    char += numbers;
+
+  } else if ( promptNum === 4) {
+    char += spclCharacters;
+
+  } else {
+    window.alert("please pick a number");
+
+  }
+
+  for (var i = 0; i <= passwordLength; i++){
+    password += char.charAt(Math.floor(Math.random() * char.length -1));
+    
+
+  }
+
+console.log(char);
+  console.log(passwordLength);
+  console.log(char);
+   return password;
+} 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
